09147bb4bc50a95962bb381267c652941dd736ec
giuseppezaccato <giuseppezaccato@gmail.com>
2025-05-28 10:42:56 +0200

    Milestone 4

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useEffect, useState } from "react";
-const { VITE_API_URL } = import.meta.env
+import { createContext } from "react";
+
+// integrazione custom hook
+import useTasks from "../hooks/useTasks";
 
 export const GlobalContext = createContext(); // Creo un nuovo contesto chiamato GlobalContext
 
@@ -7,38 +9,16 @@ export const GlobalContext = createContext(); // Creo un nuovo contesto chiamato
 // Definisco il componente provider che avvolgerà i componenti figli
 export function GlobalProvider({ children }) {
 
-    const [tasks, setTasks] = useState([]);
+    //todo tutto useState/useEffect e i relativi fetch sono finiti nel customHook
+    //* al suo posto importiamo il customHook destrutturato
 
-    //* versione async/await con useEffect annesso
-    //! (ricorda anche che funzione async NON puo stare dentro a uno useEffect,
-    //! ma l'invocazione di una funzione async SI)
-
-    // const fetchData = async () => {
-    //     try {
-    //         const data = await fetch(`${VITE_API_URL}/tasks`)
-    //         const task = await data.json()
-    //         return task //? ricordati che questa ritorna UNA PROMISE che va risolta per avere il risultato!
-    //     } catch (error) {
-    //         console.error(error)
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     fetchData().then(data => setTasks(data)); //? appunto il .then che risolve la promise di ritorno da fetchData()
-    // }, []);
-
-    // //* versione fetch standard
-    useEffect(() => {
-        fetch(`${VITE_API_URL}/tasks`)
-            .then(res => res.json())
-            .then(data => setTasks(data))
-            .catch(err => console.error(err))
-    }, []);
+    // const { tasks, addTask, removeTask, updateTask } = useTasks() //* anche un nome unico va bene!
+    const dataTasks = useTasks() //* facilita poi con lo spread il passaggio delle proprietà evitando errori di scrittura...
 
 
     return (
         // Fornisco il contesto ai componenti figli tramite il Provider
-        <GlobalContext.Provider value={{ tasks, setTasks }}>
+        <GlobalContext.Provider value={{ ...dataTasks }}>
             {children} {/* Renderizzo i componenti figli passati a GlobalProvider */}
         </GlobalContext.Provider >
     )
---
+++ src/hooks/useTasks.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+const { VITE_API_URL } = import.meta.env
+
+export default function useTask() {
+
+    const [tasks, setTasks] = useState();
+
+    //* versione async/await con useEffect annesso
+    //! (ricorda anche che funzione async NON puo stare dentro a uno useEffect, ma l'invocazione di una funzione async SI)
+    // const fetchData = async () => {
+    //     try {
+    //         const data = await fetch(`${VITE_API_URL}/tasks`)
+    //         const task = await data.json()
+    //         return task //? ricordati che questa ritorna UNA PROMISE che va risolta per avere il risultato!
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     fetchData().then(data => setTasks(data)); //? appunto il .then che risolve la promise che ritorna fetchData()
+    // }, []);
+
+    // //* versione fetch standard
+    useEffect(() => {
+        fetch(`${VITE_API_URL}/tasks`)
+            .then(res => res.json())
+            .then(data => setTasks(data))
+            .catch(err => console.error(err))
+    }, []);
+
+
+    //* operazioni per ora vuote
+    const addTask = (newTask) => {
+        //...operazioni
+    }
+    const removeTask = (taskId) => {
+        //...operazioni
+    }
+    const updateTask = (updTask) => {
+        //...operazioni
+    }
+
+
+
+    //* destructuring di quello che voglio che la funzione ritorni (comprese le operazioni)
+    return { tasks, addTask, removeTask, updateTask }
+
+
+}
